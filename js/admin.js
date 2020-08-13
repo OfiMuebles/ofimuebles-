273e7986148913b4630e89696a0388c36f1f1f1d
Jeison Lopez <yeissonllespinosa@hotmail.com>
2020-08-13 15:24:07 -0500

    Guardar productos OK.

--- js/admin.js
+++ js/admin.js
@@ -1,6 +1,9 @@
 
 
-var firebaseConfig = {
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
     apiKey: "AIzaSyBRykX5yZEriXqKduFibF8xHyjZ0KVwSG4",
     authDomain: "jeilopez.firebaseapp.com",
     databaseURL: "https://jeilopez.firebaseio.com",
@@ -11,28 +14,25 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
 var db = firebase.firestore();
-
 
 function saveproducts() {
 
 
-    //var nameproduct = document.getElementById("nameproduct").value;
-    //var priceproduct = document.getElementById("priceproduct").value;
-    //var categoryproduct = document.getElementById("categoryproduct").value;
-    //var photoproduct = document.getElementById("photoproduct").value;
-
     db.collection("products").add({
         name: nameproduct.value,
-        price: priceproduct.value
+        price: priceproduct.value,
+        category: categoryprduct.value
+        //photo : photoproduct
+
     })
-        .then((docRef) => {
-            console.log("Document written with ID:", docRef.id);
-            alert('Datos agregados correctamente', docRef.id)
-        })
-        .catch((error) => {
-            console.error("Error adding document: ", error);
-        });
-    
+       .then((docRef)=>{
+        console.log("Document written with ID:", docRef.id);
+        alert('Datos agregados correctamente', docRef.id) 
+    })
+    .catch((error)=> {
+        console.error("Error adding document: ", error);
+    });
+
 }
+
