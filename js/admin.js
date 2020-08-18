33f5c8503aaa61e00d79642e3502cdaab2963bbc
victor <v.prada@udla.edu.co>
2020-08-18 16:28:50 -0500

    feat: get diningrooms in portafolio

--- js/admin.js
+++ js/admin.js
@@ -4,13 +4,13 @@
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBRykX5yZEriXqKduFibF8xHyjZ0KVwSG4",
-    authDomain: "jeilopez.firebaseapp.com",
-    databaseURL: "https://jeilopez.firebaseio.com",
-    projectId: "jeilopez",
-    storageBucket: "jeilopez.appspot.com",
-    messagingSenderId: "465624968599",
-    appId: "1:465624968599:web:788e461716cdef34bca8d2"
+  apiKey: "AIzaSyBRykX5yZEriXqKduFibF8xHyjZ0KVwSG4",
+  authDomain: "jeilopez.firebaseapp.com",
+  databaseURL: "https://jeilopez.firebaseio.com",
+  projectId: "jeilopez",
+  storageBucket: "jeilopez.appspot.com",
+  messagingSenderId: "465624968599",
+  appId: "1:465624968599:web:788e461716cdef34bca8d2"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -19,9 +19,9 @@ var db = firebase.firestore();
 
 
 function reset() {
-    nameproduct.value = "";
-    priceproduct.value = "";
-    categoryproduct.value = "";
+  nameproduct.value = "";
+  priceproduct.value = "";
+  categoryproduct.value = "";
 
 }
 
@@ -32,76 +32,107 @@ var idProducts = document.getElementById("id");
 
 function saveproducts() {
 
-    db.collection("products").add({
-        name: nameproduct.value,
-        price: priceproduct.value,
-        category: categoryproduct.value,
-    })
-        .then((docRef) => {
-            console.log("Document written with ID:", docRef.id);
-            alert('Datos agregados correctamente', docRef.id);
-            reset();
+  db.collection("products").add({
+    name: nameproduct.value,
+    price: priceproduct.value,
+    category: categoryproduct.value,
+  })
+    .then((docRef) => {
+      console.log("Document written with ID:", docRef.id);
+      alert('Datos agregados correctamente', docRef.id);
+      reset();
 
-        })
-        .catch((error) => {
-            console.error("Error adding document: ", error);
-        });
+    })
+    .catch((error) => {
+      console.error("Error adding document: ", error);
+    });
 
 }
 
 function delet(id) {
-    db.collection("products").doc(id).delete()
-        .then(() => {
-            //console.log("Documento eliminado" +  " "+id);
+  db.collection("products").doc(id).delete()
+    .then(() => {
+      //console.log("Documento eliminado" +  " "+id);
 
-            Listproducts();
-            // location.reload();
-        }).catch((error) => {
-            console.error("Error: ", error);
-        });
+      Listproducts();
+      // location.reload();
+    }).catch((error) => {
+      console.error("Error: ", error);
+    });
 }
 
 function act() {
 
-    db.collection("products").doc(idProducts.value).update({
-        name: nameproduct1.value,
-        price: priceproduct1.value,
-        category: categoryproduct1.value
+  db.collection("products").doc(idProducts.value).update({
+    name: nameproduct1.value,
+    price: priceproduct1.value,
+    category: categoryproduct1.value
+  })
+    .then(() => {
+      console.log("Document successfully updated!");
+      Listproducts();
     })
-        .then(() => {
-            console.log("Document successfully updated!");
-            Listproducts();
-        })
-        .catch((error) => {
-            console.log("Error: ", error);
-        });;
+    .catch((error) => {
+      console.log("Error: ", error);
+    });;
 }
 
 function edit(id) {
 
-    db.collection("products").doc(id).get()
-        .then((doc) => {
+  db.collection("products").doc(id).get()
+    .then((doc) => {
 
-            Listproducts();
-            idProducts.value = id;
-            nameproduct1.value = doc.data().name;
-            priceproduct1.value = doc.data().price;
-            categoryproduct1.value = doc.data().category;
-        })
-        .catch((error) => {
-            console.log("Error: ", error);
-        });
+      Listproducts();
+      idProducts.value = id;
+      nameproduct1.value = doc.data().name;
+      priceproduct1.value = doc.data().price;
+      categoryproduct1.value = doc.data().category;
+    })
+    .catch((error) => {
+      console.log("Error: ", error);
+    });
+
+}
+
+function getdiningrooms() {
+  getdiningroom.innerHTML = "";
+  db.collection("products").where("category", "==", "COMEDORES")
+    .get()
+    .then(function (querySnapshot) {
+      querySnapshot.forEach(function (doc) {
+        getdiningroom.innerHTML += `
+              
+        <div class="col-md-3" >
+        <div class="card">
+            <img src="../img/comedor.png" alt="card-1" class="card-img-top">
+            <div class="card-body" >
+                <h5>${doc.data().name}</h5>
+                <h6>$ ${doc.data().price}</h6>
+                <button class="btn btn-danger"><i class="fa fa-cart-plus" aria-hidden="true"></i> Add To
+                    Cart</button>
+            </div>
+        </div>
+    </div>
+          
+          `;
+
+        console.log(doc.id, " => ", doc.data());
+      });
+    })
+    .catch(function (error) {
+      console.log("Error getting documents: ", error);
+    });
 
 }
 
 
 function Listproducts() {
-    listproducts.innerHTML = "";
-    //btnActualizar.classList.add('d-none');
+  listproducts.innerHTML = "";
+  //btnActualizar.classList.add('d-none');
 
-    db.collection("products").get().then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-            listproducts.innerHTML += `
+  db.collection("products").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      listproducts.innerHTML += `
                 <tr>
                         
                         <td>${doc.data().name}</td>
@@ -142,11 +173,11 @@ function Listproducts() {
                     </td>
                 </tr>           
                 `;
-        });
-    })
-        .catch(function (error) {
-            console.log("Error: ", error);
-        });
+    });
+  })
+    .catch(function (error) {
+      console.log("Error: ", error);
+    });
 
 
 }
@@ -154,12 +185,12 @@ function Listproducts() {
 function cerrarSesion() {
   console.log("Aleta")
   firebase.auth().signOut()
-      .then(() => {
-          console.log("Sesion cerrada exitosamente");
-          window.location.href = 'index.html';
-      }).catch((error) => {
-         console.log(error.message)
-      });
+    .then(() => {
+      console.log("Sesion cerrada exitosamente");
+      window.location.href = 'index.html';
+    }).catch((error) => {
+      console.log(error.message)
+    });
 }
 
 function limpiarDatosLogin() {
@@ -170,47 +201,47 @@ function verificar() {
   var user = firebase.auth().currentUser;
 
   user.sendEmailVerification().then(function () {
-      // Email sent.
+    // Email sent.
   }).catch(function (error) {
-      // An error happened.
+    // An error happened.
   });
 }
 
 function registraUsuario() {
   firebase.auth().createUserWithEmailAndPassword(emailUser.value, passUser.value)
-      .then(() => {
-          verificar();
-          console.log("El usuario se ha registrado");
-          limpiarDatosLogin();
-      })
-      .catch(function (error) {
-          console.log("Error: ", error.message);
-      }); 
+    .then(() => {
+      verificar();
+      console.log("El usuario se ha registrado");
+      limpiarDatosLogin();
+    })
+    .catch(function (error) {
+      console.log("Error: ", error.message);
+    });
 }
 
 
 function login() {
   var uno = emailUser.value;
   firebase.auth().signInWithEmailAndPassword(uno, passUser.value)
-      .then((user) => {
-          sessionStorage.setItem('login', user.email);
-          window.location.href = '../views/admin.html';
-      })
-      .catch(function (error) {
-          console.log("Error: ", error.message);
-          limpiarDatosLogin();
-      });
+    .then((user) => {
+      sessionStorage.setItem('login', user.email);
+      window.location.href = '../views/admin.html';
+    })
+    .catch(function (error) {
+      console.log("Error: ", error.message);
+      limpiarDatosLogin();
+    });
 }
 
 
 
 function estado() {
   firebase.auth().onAuthStateChanged(function (user) {
-      if (user) {
-          emailUsuarioLogueado.innerHTML = user.email;
-      }
-      else {
-          window.location.href = 'index.html';
-      }
+    if (user) {
+      emailUsuarioLogueado.innerHTML = user.email;
+    }
+    else {
+      window.location.href = 'index.html';
+    }
   });
 }
